635a07eb3dcde6a5ac172805ba5f2184fa026c18
Avery Cheng <AveryZC14@gmail.com>
2025-10-16 20:38:03 +1100

    almost finished task 3

--- A2/ass2_neo4j.py
+++ A2/ass2_neo4j.py
@@ -1,7 +1,7 @@
 ### WRITE YOUR STUDENT ID, MONASH EMAIL ADDRESS AND FULL NAME BELOW
-# STUDENT ID:
-# FULL NAME:
-# MONASH EMAIL:
+# STUDENT ID: 33905444
+# FULL NAME: Avery Cheng
+# MONASH EMAIL: ache0138@student.monash.edu
 
 #### DO NOT IMPORT ANY OTHER LIBRARIES
 import json
@@ -64,15 +64,20 @@ def process_listing(listing):
     #create the listing
     #TODO: LISTING LOCATION
     session.run("""
-        MERGE (l:Listing {id:$id, name:$name, price:$price})
+        MERGE (l:Listing {id:$id, name:$name, price:$price, location: point({longitude: $long, latitude: $lat}) })
         MERGE (n:Neighbourhood {name:$nbh})
         MERGE (l) -[:LOCATED_IN]-> (n)
         MERGE (h:Host {id:$h_id, name: $h_name, location:$h_location, is_superhost:$h_super})
         MERGE (h) -[:HOSTS]-> (l)
-    """, id=listing["id"], name=listing["name"], 
-    price=listing["price"], nbh = listing["neighbourhood"],
-    h_id=listing["host_id"], h_name=listing["host_name"],
-    h_location=listing["host_location"] or "", h_super = (listing["host_is_superhost"] == "t") )
+    """, id = listing["id"], 
+    name = listing["name"], 
+    price = listing["price"],
+    long = listing["longitude"],
+    lat = listing["latitude"],
+    nbh = listing["neighbourhood"],
+    h_id = listing["host_id"], h_name = listing["host_name"],
+    h_location = listing["host_location"] or "", 
+    h_super = (listing["host_is_superhost"] == "t"))
 
     amenities = []
     if "amenities" in listing:
@@ -91,16 +96,67 @@ def task2():
     # Find the neighbourhood with the highest number of listings, with ties broken by alphabetical order of the neighbourhood name.
     # Print the most expensive listing in the neighbourhood
     res = session.run("""
-        MATCH (l:Listing) -[:LOCATED_IN]-> (n:Neighbourhood)
-        return n.name, COUNT(l)
+        MATCH (l1:Listing) -[:LOCATED_IN]-> (n:Neighbourhood)
+        //keep popular_n as a variable for the best
+        WITH n as popular_n, COUNT(l1) as n_count
+        ORDER BY COUNT(l1) DESC, n.name ASC
+        LIMIT 1
+        
+        MATCH (l2:Listing) -[:LOCATED_IN]-> (popular_n)
+        RETURN l2.id as l_id, 
+            l2.name as l_name, 
+            l2.price as l_price, 
+            popular_n.name as n_name, 
+            n_count
+        ORDER BY l2.price DESC
+        LIMIT 1
+        
     """)
-    print_results(res)
-    # pass
+    
+    #single() gets just one record out of a result, dict() turns the items of that record into a dictionary
+    result = res.single()
+    result_dict = dict(result.items())
+    
+    listing_id = result_dict["l_id"]
+    listing_name = result_dict["l_name"]
+    listing_price = result_dict["l_price"]
+    neighbourhood_name = result_dict["n_name"]
+    neighbourhood_listing_count = result_dict["n_count"]
+    
+    print(
+        f"""Neighbourhood with the highest number of listings: {neighbourhood_name} (Total Listings: {neighbourhood_listing_count})
+Most expensive listing in {neighbourhood_name}:
+Listing: ID: {listing_id}, Name: {listing_name}, Price: ${listing_price}
+        """
+    )
 
 def task3(neighbourhood, distance_km):
     # Find the most expensive listing in the specified neighbourhood, with ties broken by listing_id (smaller is better).
     # Find other listingsss within distance_km of the most expensive listing and print the 3-lowest priced ones, with ties broken by distance
-    pass
+    res = session.run("""
+        MATCH (l1:Listing) -[:LOCATED_IN]-> (n:Neighbourhood {name:$nbh}),
+            (h1:Host) -[:HOSTS]-> (l1)
+        WITH l1, h1
+        ORDER BY l1.price DESC, l1.id ASC
+        LIMIT 1
+        
+        MATCH (l2:Listing), (h2:Host) -[:HOSTS]-> (l2)
+        WHERE point.distance(l1.location, l2.location) < $max_dist
+        RETURN l1.name as l1_name, 
+            l1.id as l1_id, 
+            l1.price as l1_price, 
+            h1.name as h1_name,
+            l2.name as l2_name,
+            l2.id as l2_id, 
+            l2.price as l2_price, 
+            h2.name as h2_name,
+            round(point.distance(l1.location, l2.location)/1000, 2) as dist
+        ORDER BY l2.price ASC, point.distance(l1.location, l2.location) ASC, l2.id ASC
+        LIMIT 3
+    """,
+    nbh = neighbourhood,
+    max_dist = distance_km*1000)
+    print_results(res)
 
 
 # Main function that controls the flow of the script
@@ -113,7 +169,7 @@ def main():
         # Uncomment the following line to recreate the database from scratch
         # recreate_database()
         task2()
-        #task3("Melbourne", 5)
+        task3("Melbourne", 5)
 
 # Run the main function
 if __name__ == "__main__":
